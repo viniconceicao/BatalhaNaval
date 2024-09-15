01d6a3a656e241b7d426a7483e99a2f41425ef83
skky <viniciusdelizconceicao@gmail.com>
2024-09-14 23:51:03 -0300

    Mudando

--- src/Main.kt
+++ src/Main.kt
@@ -6,9 +6,14 @@ fun main() {
     // Jogando informação para o terminal
     for ( tabuleiro in matriz2D) {
         for ( elemento in tabuleiro){
+            // Posicionar algo
+            matriz2D[2][3] = 'N'
+            //
             print("$elemento")
         }
+        // Saída
         println()
     }
+
 
 }
