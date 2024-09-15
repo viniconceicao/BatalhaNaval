8bfe8bb6196427f34a3c0fe7b15db528c689ae32
skky <viniciusdelizconceicao@gmail.com>
2024-09-15 00:37:20 -0300

    Mudando amanhã

--- src/Main.kt
+++ src/Main.kt
@@ -1,13 +1,15 @@
 fun main() {
-
     // Criando a matriz
-    val matriz2D = Array(10) {CharArray(10){ ' '} }
+    val matriz2D = Array(10) {CharArray(10){ ' ' } }
+
+    val cruzador = "\uD83E\uDD14"
+    val c: String = cruzador
 
     // Jogando informação para o terminal
     for ( tabuleiro in matriz2D) {
         for ( elemento in tabuleiro){
             // Posicionar algo
-            matriz2D[2][3] = 'N'
+            matriz2D[2][3] = c[0]
             //
             print("$elemento")
         }
